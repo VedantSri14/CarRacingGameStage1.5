04b95d4a58ef0aeee6c85069ea769d935ad5e922
VedantSri14 <srivastavavishal.vs@gmail.com>
2020-12-21 19:02:35 +0530

    Added colored cars!

--- sketch.js
+++ sketch.js
@@ -9,23 +9,31 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
-
-
-function setup(){
-  canvas = createCanvas(displayWidth - 20, displayHeight-30);
-  database = firebase.database();
-  game = new Game();
-  game.getState();
-  game.start();
+function preload() {
+  car1img = loadImage("images/car1.png")
+  car2img = loadImage("images/car2.png");
+  car3img = loadImage("images/car3.png");
+  car4img = loadImage("images/car4.png");
+  groundImg = loadImage("images/ground.png");
+  trackImg = loadImage("images/track.jpg");
+}
+function setup() {
+	canvas = createCanvas(displayWidth - 20, displayHeight - 30);
+	database = firebase.database();
+	game = new Game();
+	game.getState();
+	game.start();
 }
 
-
-function draw(){
-  if(playerCount === 4){
-    game.update(1);
+function draw() {
+	if (playerCount === 4) {
+		game.update(1);
+	}
+	if (gameState === 1) {
+		clear();
+		game.play();
   }
-  if(gameState === 1){
-    clear();
-    game.play();
+  if(gameState === 2) {
+    game.end()
   }
 }
